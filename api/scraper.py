a14711bc1cebe2c78ce81c9d9296c8d181bbee78
pradnyalgandhi <pradnyalgandhi28@gmail.com>
2021-03-16 02:16:50 +0530

    Added apps, Clean sheets, Goals, Wins & Losses scraper and update jersey and position scraper

--- api/scraper.py
+++ api/scraper.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import pprint
+import pprint, re
 import requests
 
 url = "https://www.premierleague.com/players/"
@@ -47,26 +47,33 @@ class PlayerStats:
         self.url = url
         self.player_urls = []
         self.player_stats_urls = []
+        self.player_position = []
 
         req = requests.get(url)
         self.soup = BeautifulSoup(req.content,'html.parser')
+
+        #Extracting player URLs
         tags = self.soup.find_all('a', class_='playerName')
 
         for url in tags:            
 
             player_url = "https://www.premierleague.com"+ url.get('href')
-            self.player_urls.append(player_url)
+            self.player_urls.append(player_url)            
 
             temp = player_url.split("/")[:-1]
             stats_url = f"{'/'.join(temp)}/stats"
-            self.player_stats_urls.append(stats_url)        
-    
+            self.player_stats_urls.append(stats_url)
+
+        #Extracting player Position      
+        position = self.soup.find_all('td', class_='hide-s')
+        for pos in position:            
+            temp = re.search(r'(\"\>)(\w*)(\<\/td)', str(pos))
+            if temp:         
+                posi = temp.group(2)
+                self.player_position.append(posi)            
+ 
     def get_type(self):
         player_type = self.soup.find("td", class_="hide-s").get_text()
-        return player_type
-
-    def get_player_type(self):
-        player_type = self.soup.find_all('div', class_="info")[1].get_text()
         return player_type
 
     def get_name(self):
@@ -74,23 +81,42 @@ class PlayerStats:
         return player_name
 
     def get_jersey_number(self):
-        player_jersey_number = self.soup.find_all('div', class_="t-colour")        
-        return player_jersey_number[0].get_text()
+        player_jersey_number = self.soup.find('div', class_="number t-colour")
+        if player_jersey_number:
+            return player_jersey_number.get_text()
+        else:
+            return "Not assigned"
 
     def get_club(self):
-        pass
+        
+        player_club = self.soup.find('div', class_="info")        
+        if player_club:
+            player_club = player_club.get_text().replace(" ","").replace("\n","")
+            if (player_club == 'Forward' or player_club == "Midfielder" or player_club == "Defender" or player_club == "Goalkeeper"):
+                return "Not Available"
+            else:
+                return player_club
+        else:
+            "Not Available"
 
     def get_nationality(self):
-        player_nationality = self.soup.find('span', class_="playerCountry").get_text()
-        return player_nationality
-
+        player_nationality = self.soup.find_all('span', class_="playerCountry")
+        if player_nationality:
+            return player_nationality[0].get_text()
+        else:
+            return "Not Available"
+    
     def get_apperances(self):
-        player_appearances = self.soup.find('span', class_="allStatContainer statappearances").get_text()
+        player_appearances = self.soup.find('span', class_="allStatContainer statappearances").get_text().replace(" ","").replace("\n","")
         return player_appearances
 
     def get_goals(self):
         player_goals = self.soup.find('span', class_="allStatContainer statgoals").get_text()
         return player_goals
+
+    def get_clean_sheets(self):
+        player_clean_sheets = self.soup.find('span', class_="allStatContainer statclean_sheet").get_text()
+        return player_clean_sheets
 
     def get_wins(self):
         player_wins = self.soup.find('span', class_="allStatContainer statwins").get_text()
@@ -129,19 +155,33 @@ class PlayerStats:
             self.soup = soup_overview
 
             player_info['Name'] = self.get_name()
-            player_info['Jersey No'] = self.get_jersey_number()
+            player_info['Current Club'] = self.get_club()      
             player_info['Nationality'] = self.get_nationality()
-            
+            player_info['Position'] = self.player_position[i]
+            player_info['Jersey No'] = self.get_jersey_number()            
             player_info['Career'] = self.get_career_stats()
+
+            req = requests.get(self.player_stats_urls[i])
+            soup_stats = BeautifulSoup(req.content, 'html.parser')
+            self.soup = soup_stats
+
+            player_info['Appearances'] = self.get_apperances()
+            if self.player_position[i] == 'Goalkeeper':
+                player_info['Clean Sheets'] = self.get_clean_sheets().replace(" ", "").replace("\n","")
+            else:
+                player_info['Goals'] = self.get_goals().replace(" ", "").replace("\n","")
+            player_info['Total Wins'] = self.get_wins().replace(" ", "").replace("\n","")
+            player_info['Total Losses'] = self.get_losses().replace(" ", "").replace("\n","")
 
             player.append(player_info)
         return player
         
-# pp = pprint.PrettyPrinter()
-# inst = PlayerStats(url)
-# pp.pprint(inst.create_instance())
+pp = pprint.PrettyPrinter()
+inst = PlayerStats(url)
+pp.pprint(inst.create_instance())
 
 
-# url1 = "https://www.premierleague.com/players/13286/Tammy-Abraham/overview"
+# url1 = "https://www.premierleague.com/players/"
 # inst = PlayerStats(url1)
-# print(inst.get_jersey_number())
+# print(len(inst.player_position))
+
